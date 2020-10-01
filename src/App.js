72a0c9b909a81b9912b19066551532976dea23fc
Gregory Ferreira <gregoryflucena@gmail.com>
2020-10-01 13:51:38 -0300

    Itens redenrizados na tela

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Movie from './components/movies';
 
-function App() {
-  return (
-    <div className="App">
+const api_url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const api_search =  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(async () => {
+    fetch(api_url)
+    .then((res) => res.json())
+    .then((data) => {
+      setMovies(data.results);
+    });
+  }, [])
+
+  return (
+    <div className="movie-container">
+      {movies.length > 0 && 
+        movies.map((movie) =>
+        <Movie key={movie.id} {...movie} />
+      )}
     </div>
   );
 }
--- src/components/movies.js
+++ src/components/movies.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const Movie = () => {
+const api_img = "https://image.tmdb.org/t/p/w1280";
+
+const Movie = ({title, poster_path, overview, vote_average}) => {
   return (
-    <div className="Movie">
-      
+    <div className="movie">
+      <img src={api_img + poster_path} alt={title} />
+
+      <div className="movie-info">
+        <h3>{title}</h3>
+
+        <span>{vote_average}</span>
+      </div>
+
+      <div className="movie-over">
+        <h2>Overview</h2>
+        <p>{overview}</p>
+      </div>
     </div>
   );
 }
